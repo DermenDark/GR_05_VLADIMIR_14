3648c650e171fe4f48f02e2624325dd7113bec6b
ShirkoVladimir <volodya.shirko@gmail.com>
2026-05-22 04:47:31 +0300

    IGI_LR5 Final

---
+++ IGI/LR5/Test_project/WeatherApp/weather/views.py
@@ -0,0 +1,24 @@
+import requests
+from django.shortcuts import render
+from .models import City
+
+def index(request):
+    appid = '63026ab922acdc159af4d7d5c6adca54'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    cities = City.objects.all()
+    all_info = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+
+        if res.get("main"):
+            city_info = {
+                'city': city.name,
+                'temp': res["main"]["temp"],
+                'icon': res["weather"][0]["icon"]
+            }
+            all_info.append(city_info)
+
+    context = {'all_info': all_info}
+    return render(request, 'weather/index.html', context)
