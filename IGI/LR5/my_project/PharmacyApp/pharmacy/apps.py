2f7d1a7634256099d75fad9076f5657a6631c8aa
DemenDark <volodya.shirko@gamil.com>
2026-05-25 01:01:27 +0300

    IGI_LR5 change file location project

--- IGI/LR5/my_project/PharmacyApp/pharmacy/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PharmacyConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "pharmacy"
-
-    def ready(self):
-        from . import signals
--- IGI/LR5/my_project/PharmacyApp/stats_graf/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from . import views
-
-app_name = "stats_graf"
-
-urlpatterns = [
-    re_path(r'^$', views.stats_view, name='stats'),
-]
